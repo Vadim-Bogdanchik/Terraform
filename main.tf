f5a7906799c741ea86d07af48cf4f01318bd3d8e
Vadim Bogdanchik <bogdanchik270978@gmail.com>
2022-01-20 09:58:51 +0300

    first commit

---
+++ main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+      version = "0.61.0"
+    }
+  }
+}
+
+provider "yandex" {
+  token     = "AQAAAAAWnIK7AATuwf7FuXRNEEGfl2v1g4Y2VKA"
+  cloud_id  = "b1gce3ouctr3f2g9u7l4"
+  folder_id = "b1g8t3bjiia4btu74dmc"
+  zone      = "ru-central1-c"
+}
+
+
+
+/****** настройки провайдера ********/
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "touristerra1"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81hgrcv6lsnkremf32"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("C:/DevOps/Terraform/YC-KEY.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "touristerra2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd81hgrcv6lsnkremf32"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("C:/DevOps/Terraform/YC-KEY.pub")}"
+  }
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
